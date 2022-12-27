fff8437c397c9331e8d45cb8023bdfd03545836d
Mark Sakhatskyi <mark.sahaxkiy@gmail.com>
2022-12-27 13:35:06 +0200

    10hw maybe done

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,26 +3,30 @@ import Notiflix from "notiflix";
 const countryList = document.querySelector('.country-list');
 countryList.style.listStyle = 'none';
 const countryInfo = document.querySelector('.country-info');
+countryInfo.style.listStyle = 'none';
 const input = document.querySelector('#search-box');
 // console.dir(input);
+function ZEROINGHTML() {
+    countryList.innerHTML = " ";
+    countryInfo.innerHTML = " ";
+};
 let name = null;
-let country = 0;
-let countrys = [];
+
+
 const URL = 'https://restcountries.com/v2/name/';
 
 export default function fetchCountries(name) {
     name = input.value;
-    // name.thim();
+
     if (name == 0) {
+        ZEROINGHTML();
         return;
     }
     fetch(`${URL}${name}?fields=name,capital,population,flags,languages`).then(
         (r) => {
             if (!r.ok) {
-                throw new Error(r.status);
+                Notiflix.Notify.failure('Oops, there is no country with that name');
             }
-            // isFetchCountriesLengs();
-          
             r.json().then(function (data) {
                 console.log('data', data.length)
                 console.log(data);
@@ -30,10 +34,20 @@ export default function fetchCountries(name) {
                  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
                 }
                 if (data.length > 2 && data.length <= 10) {
-                        const markup = data.map(({flags, name}) => 
-                            `<li><img src="${flags.svg}"alt ="flags ${name}" width = 20> ${name}</li>`).join("");
-                        countryList.insertAdjacentHTML("afterbegin", markup);
-                        console.log(markup);
+                    ZEROINGHTML();
+                        const markupCoutryList = data.map(({flags, name}) => 
+                            `<li class = "list__item"><img src="${flags.svg}"alt ="flags ${name}" width = 20> ${name}</li>`).join("");
+                    countryList.insertAdjacentHTML("afterbegin", markupCoutryList);
+                }
+                if (data.length == 1) {
+                    ZEROINGHTML();
+                    const markupCoutryList = data.map(({ flags, name}) =>
+                        `<li class = "list__item"><img src="${flags.svg}"alt ="flags ${name}" width = 20>${name}</li>`).join("");
+                    countryList.insertAdjacentHTML("afterbegin", markupCoutryList);
+                    const markupCoutryInfo = data.map(({capital, population, languages }) =>`<li class= "list__item">Capital:${capital}</li>
+                    <li class= "list__item">population:${population}</li>
+                    <li class = "list__item">languages:${languages[0].name}</li>`)
+                    countryInfo.insertAdjacentHTML("afterbegin" ,markupCoutryInfo)
                 }
             });
 
@@ -41,16 +55,3 @@ export default function fetchCountries(name) {
     );
 };
 
-
-// function isFetchCountriesLengs() {
-//     fetchCountries().then(console.log);
-//     for (country of countrys){
-//         country += 1;
-//     if (country > 10) {
-//         return console.log('false');
-//     }
-//   }
-        
-//         return console.log('true');
-    
-// }
