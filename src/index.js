37011592d1b7cf2d1b3e0e2a1f34466ad33c3df8
Mark Sakhatskyi <mark.sahaxkiy@gmail.com>
2022-12-29 08:37:52 +0200

    10hw mentor comentar done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {fetchCountries} from './js/fetchCountries';
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-export { ZEROINGHTML };
+export { zeroingHTML };
 const DEBOUNCE_DELAY = 300;
 
 const countryList = document.querySelector('.country-list');
@@ -11,7 +11,7 @@ const countryInfo = document.querySelector('.country-info');
 countryInfo.style.listStyle = 'none';
 const input = document.querySelector('#search-box');
 
-function ZEROINGHTML() {
+function zeroingHTML() {
     countryList.innerHTML = " ";
     countryInfo.innerHTML = " ";
 };
@@ -19,17 +19,18 @@ function ZEROINGHTML() {
 input.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
     
 export default function renderList(data) {
+    
         if (data.length > 10) {
          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         }
         if (data.length > 2 && data.length <= 10) {
-            ZEROINGHTML();
+            zeroingHTML();
              const markupCoutryList = data.map(({flags, name}) => 
                     `<li class = "list__item"><img src="${flags.svg}"alt ="flags ${name}" width = 20> ${name}</li>`).join("");
             countryList.insertAdjacentHTML("afterbegin", markupCoutryList);
         }
     if (data.length == 1) {
-        ZEROINGHTML();
+        zeroingHTML();
          const markupCoutryList = data.map(({flags, name}) => 
                     `<li class = "list__item"><img src="${flags.svg}"alt ="flags ${name}" width = 20> ${name}</li>`).join("");
             countryList.insertAdjacentHTML("afterbegin", markupCoutryList);
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 export { fetchCountries };
 import Notiflix from "notiflix";
 import renderList from "../index";
-import {ZEROINGHTML} from "../index";
+import {zeroingHTML} from "../index";
 
 const input = document.querySelector('#search-box');
 let name = null;
@@ -9,16 +9,18 @@ let name = null;
 const URL = 'https://restcountries.com/v2/name/';
 
 function fetchCountries(name) {
-    name = input.value;
+    name = input.value.trim();
 
-    if (name == 0) {
-        ZEROINGHTML();
+    if (name.length === 0) {
+        zeroingHTML();
         return;
     }
+    
     fetch(`${URL}${name}?fields=name,capital,population,flags,languages`).then(
         (r) => {
             if (!r.ok) {
                 Notiflix.Notify.failure('Oops, there is no country with that name');
+                zeroingHTML();
             }
            return r.json()
 
